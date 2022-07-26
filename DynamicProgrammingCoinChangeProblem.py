2d932975510b6d2a4c2281417149e5495ca3fbdc
Younis <younisrahman007@gmail.com>
2022-07-26 13:36:51 +0600

    dp

--- DynamicProgrammingCoinChangeProblem.py
+++ DynamicProgrammingCoinChangeProblem.py
@@ -10,15 +10,18 @@ def minCoin(n, arr, dp):
     for i in range(len(arr)):
         if n-arr[i] >= 0:
             if dp[n-arr[i]] != -1:
-
-            subAns = minCoin(n - arr[i], arr, dp)
+                subAns = dp[n-arr[i]]
+            else:
+                subAns = minCoin(n - arr[i], arr, dp)
             if subAns != sys.maxsize and subAns + 1 < ans:
                 ans = subAns + 1
+    dp[n] = ans
     return ans
 
 
 n = 18
 arr = [7, 5, 1]
-dp = [n+1]*-1
+dp = [-1]*(n+1)
 dp[0] = 0
 print(minCoin(n, arr, dp))
+print(dp)
