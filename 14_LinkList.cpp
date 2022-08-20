18a452afbca469d36b63cd3f8bc61ee36e5a7793
Younis Rahman <younisrahman007@gmail.com>
2022-08-20 14:07:38 +0600

    New PC

--- 14_LinkList.cpp
+++ 14_LinkList.cpp
@@ -1,27 +1,13 @@
-
 #include <bits/stdc++.h>
-// #include <iostream>
-
+//#include<iostream>
 using namespace std;
-class LinkedList
+class Node
 {
 public:
-    int age;
-    double cgpa;
-    string name;
-    LinkedList()
-    {
-        age = 10;
-        name = "name";
-        cgpa = 3.9;
-    }
-};
-int main()
+    int data;
+    Node *next;
+} int main()
 {
-    LinkedList yr(), rr();
-    // yr.name = "Younis";
-    // rr.name = "Rahman";
-    // cout << yr.name << endl;
-    // cout << rr.name << endl;
+    Node *head = Null;
     return 0;
 }
