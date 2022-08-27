50d645672c11b13cf24ac390cb6c262bd95d671d
Younis Rahman <younisrahman007@gmail.com>
2022-08-27 11:12:18 +0600

    circular

---
+++ 18A_CircularLinkedList.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+using namespace std;
+
+class node
+{
+
+public:
+    int data;
+    node *next;
+
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+void insertAtHead(node *&head, int val)
+{
+    node *n = new node(val);
+    if (head == NULL)
+    {
+        n->next = n;
+        head = n;
+        return;
+    }
+
+    node *temp = head;
+
+    while (temp->next != head)
+    {
+        temp = temp->next;
+    }
+    temp->next = n;
+    n->next = head;
+    head = n;
+}
+void display(node *head)
+{
+
+    node *temp = head;
+    do
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    } while (temp != head);
+    cout << endl;
+}
+
+int main()
+{
+
+    node *head = NULL;
+    insertAtHead(head, 3);
+    insertAtHead(head, 5);
+    insertAtHead(head, 8);
+    display(head);
+
+    return 0;
+}
